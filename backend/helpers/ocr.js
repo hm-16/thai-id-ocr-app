3581e0a6ec05deb4b22ff568cc5e502934eba67d
hm-16 <harshul2k@gmail.com>
2023-12-25 18:02:44 +0530

    Google API Key changes

--- backend/helpers/ocr.js
+++ backend/helpers/ocr.js
@@ -4,19 +4,7 @@ const extractInformation = require('./convertText');
 
 // Google Cloud Vision API client
 const vision = new ImageAnnotatorClient({
-    keyFilename: {
-            "type": process.env.type,
-            "project_id": process.env.project_id,
-            "private_key_id": process.env.private_key_id,
-            "private_key": process.env.private_key,
-            "client_email": process.env.client_email,
-            "client_id": process.env.client_id,
-            "auth_uri": process.env.auth_uri,
-            "token_uri": process.env.token_uri,
-            "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-            "client_x509_cert_url": process.env.client_x509_cert_url,
-            "universe_domain": process.env.universe_domain
-    },
+    keyFilename: 'credentials\\thai-id-card-ocr-a0713ee030c0.json',
 });
 
 async function performOCR(file){
